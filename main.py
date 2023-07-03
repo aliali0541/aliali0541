8dd038e19f09a51ee5d37d52ab0274a2fa05b2be
Ali <zeroa0541@gmail.com>
2023-07-03 10:49:24 +0300

    added py

---
+++ main.py
@@ -0,0 +1,8 @@
+name = "Ali"
+age = 205273273278
+lang = "python"
+college = "NSST"
+
+
+print("\n Hello World!, My Name Is {}, I'm studing At {} faculty, My Age Is {:,d}, I'm Learning {} with EL Zero Web School " .format(
+    name, college, age, lang))
