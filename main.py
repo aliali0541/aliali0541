c3c5286e7392227914b5050674f0e0cf7a55f8dd
Ali <zeroa0541@gmail.com>
2023-07-03 10:52:48 +0300

    file was removed

--- main.py
+++
@@ -1,8 +0,0 @@
-name = "Ali"
-age = 205273273278
-lang = "python"
-college = "NSST"
-
-
-print("\n Hello World!, My Name Is {}, I'm studing At {} faculty, My Age Is {:,d}, I'm Learning {} with EL Zero Web School " .format(
-    name, college, age, lang))
